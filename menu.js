fd534060e6f940178da2a5007f3db12417f2b3d2
Albert <alberto.castro1987@gmail.com>
2021-10-05 21:11:06 -0500

    added hover for contact in toggle menu

--- menu.js
+++ menu.js
@@ -2,6 +2,5 @@ const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav-side-menu");
 
 hamburger.addEventListener("click", () => {
-  console.log("hello");
   nav.classList.toggle("show");
 });
